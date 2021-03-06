fd09f4ab36643d8e4de30286c8d08f2aecfc7f79
John Gilday <jgilday01@gmail.com>
2021-03-06 16:41:43 -0500

    start changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid p-0">
+      <div className="row">
+        <div className="col-12">
+
+          <div className="p-3">
+            <h1>ABOUT ME</h1>
+            <img src={'/images/profile-pic.jpg'} className='rounded-img float-end' />
+
+            <p>
+              I have a strong passion for collaboration and creativity. I feel there is something special about taking a concept and then turning it into a reality.
+              </p>
+            <p>
+              In 2015 I completed a Web Development Certification
+              at Delaware County Commuinty College learning web fundamentals like HTML, CSS, PHP and Bootstrap.
+              I then started working in Web Development and as a Software Developer in May of 2016.
+              In 2020 I completed the Front End and Mobile Development Certification at Nucamp working with REACT and REACT NATIVE.
+              </p>
+            <p>
+              I have worked in Information Technology since 2007.
+              Working in Web Development has been my favorite part of working in IT.
+              Programming has allowed me to apply creative skills which I truly enjoy.
+              </p>
+            <p>
+              I also combine my interests in art, music and gaming by working in Unity 3d doing their tutorials and eventually building my own games. This gave me a chance to incorporate sound, music, graphics and also some coding logic.
+              </p>
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 }
