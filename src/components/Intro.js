19c94903c5c49ffd3e20c20db79d9201b0964cb3
John Gilday <jgilday01@gmail.com>
2021-04-17 15:26:18 -0400

    intro change

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -5,12 +5,11 @@ function Intro() {
         <div className="align-self-center mx-3 mx-md-5 my-2 p-4 header-wrapper">
             <ScrollAnimation animateIn='fadeIn' delay={1000} duration={5} animateOnce={true}>
 
-                <div className="display-2">
+                <div className="display-1">
                     Hello, I'm <strong>John Gilday</strong>
                 </div>
-                <div className="display-6">
-                    A passionate software developer who is team oriented
-                    and driven by creativity and collaboration.
+                <div className="display-5">
+                    A software developer with 5+ years experience in development and a passion for creativity and collaboration.
                 </div>
             </ScrollAnimation>
         </div>
